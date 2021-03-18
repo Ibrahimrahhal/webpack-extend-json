46ec064347abec446f9d576f0a260083cf7ebebf
Ibrahim Rahhal <ibrahim.rahhal3636@gmail.com>
2021-03-18 23:45:21 +0200

    issue-1: Loader should work in basic scenario (#7)
    Co-authored-by: Ibrahim Rahhal <ibrahimrahhal@Ibrahims-MacBook-Pro.local>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
+import * as path from 'path';
+import * as fs from 'fs';
+
+const resolveJSON = (_path:string, content?:string) => {
+    if(!content) content = fs.readFileSync(_path, 'utf8');
+    const jsonContent = JSON.parse(content);
+    if(!jsonContent.extends) return content;
+    if(!path.isAbsolute(jsonContent.extends)) jsonContent.extends = path.resolve(path.join(_path, "../", jsonContent.extends));
+    return JSON.stringify({
+        ...JSON.parse(resolveJSON(jsonContent.extends)),
+        ...(jsonContent.data || {})
+    });
+}
 const loader = function (content:string) :string {
-    return content;
+    return resolveJSON(this.resourcePath, content);
 }
 export default loader;
 module.exports = loader;
